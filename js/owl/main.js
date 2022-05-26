3c5c5e52c58df1618384f9c951c88722bcd8d2ab
rauldangio <jojofagqwrt@gmail.com>
2022-05-26 20:25:06 -0300

    colocando o OwlCarousel2

---
+++ js/owl/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:8
+        }
+    }
+})
